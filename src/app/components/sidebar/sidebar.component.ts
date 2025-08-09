f939020f5c666a7a86fe5f3cf08d9b8e50eebd58
peterramirez18 <peterramirez1812@gmail.com>
2025-08-09 09:50:15 -0400

    `Added animations to sidebar component and updated dependencies in package-lock.json and package.json`

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,6 +11,15 @@ import { IconTransactionsComponent } from '../../icons/icon-transactions.compone
 import { IconBudgetsComponent } from '../../icons/icon-budgets.component';
 import { IconPotsComponent } from '../../icons/icon-pots.component';
 import { IconRecurringBillsComponent } from '../../icons/icon-recurring-bills.component';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-sidebar',
@@ -24,6 +33,22 @@ import { IconRecurringBillsComponent } from '../../icons/icon-recurring-bills.co
     CommonModule,
   ],
   templateUrl: './sidebar.component.html',
+  animations: [
+    trigger('toggleWidth', [
+      state('expanded', style({ width: '240px' })),
+      state('collapsed', style({ width: '80px' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('220ms cubic-bezier(.2,.8,.2,1)')
+      ),
+    ]),
+
+    trigger('rotate', [
+      state('expanded', style({ transform: 'rotate(0deg)' })),
+      state('collapsed', style({ transform: 'rotate(180deg)' })),
+      transition('expanded <=> collapsed', animate('180ms ease')),
+    ]),
+  ],
 })
 export class SidebarComponent {
   // state
